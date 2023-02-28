2336135ccc45601931f047034b60830e36b8857f
Nivek <vallhalla@qq.com>
2023-02-28 14:04:21 +0800

    Set tailwind style

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,10 +1,53 @@
 <script setup>
-import AccountForm from "./AccountForm.vue";
+import {computed, ref} from "vue";
+import axios from "axios";
+import FormInputItem from "./FormInputItem.vue";
+import {User, Key} from '@element-plus/icons-vue'
+import '@material/web/button/filled-button'
+
+
+const userName = ref('')
+const password = ref('')
+
+const loginDisabled = computed(() => userName.value === "" || password.value === "")
+
+
+async function login() {
+  try {
+    const response = await axios.get('/log', {
+      params: {
+        Acc: userName.value,
+        Pwd: password.value
+      }
+    })
+    console.log(response.data)
+  } catch (e) {
+    console.log(e)
+  }
+
+}
+
+const prompt = "请输入用户名"
 </script>
 
 <template>
-  <div class="flex flex-col justify-center w-full ">
-    <AccountForm/>
+  <div class="flex flex-col justify-center min-h-screen items-center">
+
+    <FormInputItem label="User Name">
+      <template v-slot:icon>
+        <User style="width: 1em; height: 1em; margin-right: 8px"/>
+      </template>
+    </FormInputItem>
+
+    <FormInputItem label="Password">
+      <template v-slot:icon>
+        <Key style="width: 1em; height: 1em; margin-right: 8px"/>
+      </template>
+    </FormInputItem>
+
+  <md-filled-button label="登录" @click="login" class="p-5 w-44" > </md-filled-button>
+
+
   </div>
 
 
